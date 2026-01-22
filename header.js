d19dd2b161ca8d29824ce5ae8dfe8f34415805d9
Pablo Pérez <165832675+papgar92@users.noreply.github.com>
2026-01-22 21:30:26 +0100

    Update link for Referencias to open in new tab

--- header.js
+++ header.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     <div class="contact-links">
                         <a href="https://linkedin.com/in/ppg92" target="_blank">LinkedIn</a>
                         <a href="resources/CV PPG.pdf" download="CV-Pablo-Perez-Garcia-SOC.pdf">Curriculum Vitae</a>
-                        <a href="resources/Recomendacion.pdf" download="Referencias-Pablo-Perez-Garcia.pdf">Referencias</a>
+                        <a href="resources/Recomendacion.pdf" target="_blank">Referencias</a>
                     </div>
                 </div>
             </div>
